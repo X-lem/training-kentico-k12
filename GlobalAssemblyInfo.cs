424e7f03b2096a1d001922fd51930add42311cdc
X-lem <X-lem@hotmail.com>
2019-05-06 11:39:44 -0700

    Applied hotfix 12.0.21

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("12.0.0.0")]
-[assembly: AssemblyFileVersion("12.0.7034.28880")]
-[assembly: AssemblyInformationalVersion("12.0.17")]
+[assembly: AssemblyFileVersion("12.0.7062.28878")]
+[assembly: AssemblyInformationalVersion("12.0.21")]
